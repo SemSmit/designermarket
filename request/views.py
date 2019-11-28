b2c32c7a4fb20f0e328c974647c69d01197ffef5
Ubuntu <ubuntu@ip-172-31-33-93.eu-central-1.compute.internal>
2019-11-28 18:38:15 +0000

    Registrating with role works, html pages respond with UserProfile

--- request/views.py
+++ request/views.py
@@ -2,10 +2,18 @@ from django.shortcuts import render, redirect, HttpResponseRedirect
 from django.contrib import messages, auth
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
+from accounts.models import UserProfile
+
 
 # Create your views here.
 @login_required
 def requestview(request):
-    """A view that displays the request page"""
-    return render(request, "request.html")
+    current_user = request.user
+    if current_user.userprofile.role == "Designer":
+        return render(request, "designs.html")
+    elif current_user.userprofile.role == "User":
+        return render(request, "home.html")
+    else: 
+        """A view that displays the request page"""
+        return render(request, "request.html")
         
